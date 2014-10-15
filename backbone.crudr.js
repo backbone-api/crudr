8a646ecbe50409220cfb7e6986b9ff5dbdca45f7
Makis Tracend <makis.tracend@gmail.com>
2014-10-15 00:50:47 -0700

    upgrading logic for CRUDr > 0.3.0

--- backbone.crudr.js
+++ backbone.crudr.js
@@ -1,20 +1,20 @@
 (function(Backbone, _){
-	
+
 var origSync = Backbone.sync;
 
 var Helpers = {
 	// Backbone.js support
 	// Listen for backend notifications and update the
 	// collection models accordingly.
-    
+
 	bindBackend: function() {
 		var self = this;
-		   
-		if( ( typeof(this.model) == "undefined" ) ) { 
+
+		if( ( typeof(this.model) == "undefined" ) ) {
 			// this is a model
 			this.backend.ready(function() {
 				var event = self.backend.options.event;
-				
+
 				self.bind(event + ':create', function(response) {
 					self.parse(response);
 				});
@@ -25,17 +25,17 @@ var Helpers = {
 					self.destroy();
 				});
 			});
-			
-			
+
+
 		} else {
 			// this is a collection
-			
-            //var idAttribute = ( typeof(this.model) == undefined ) ? this.prototype.idAttribute : this.model.prototype.idAttribute;
+
+			//var idAttribute = ( typeof(this.model) == undefined ) ? this.prototype.idAttribute : this.model.prototype.idAttribute;
 			var idAttribute = this.model.prototype.idAttribute || this.prototype.idAttribute;
-			
+
 			this.backend.ready(function() {
 				var event = self.backend.options.event;
-					
+
 				self.bind(event + ':create', function(model) {
 					self.add(model);
 				});
@@ -47,9 +47,9 @@ var Helpers = {
 					self.remove(model[idAttribute]);
 				});
 			});
-			
+
 		}
-	}  
+	}
 };
 
 
@@ -67,68 +67,69 @@ function inherit(Parent, Child, mixins) {
 
 	return _.extend(Child, Parent);
 };
-    
+
 Backbone.sync = function(method, model, options) {
 	var backend = model.backend || (model.collection && model.collection.backend);
-	
-    if (backend) {
-		
-        var error = options.error || function() {};
+
+	if (backend) {
+
+		var error = options.error || function() {};
 		var success = options.success || function() {};
-		
+
 		// Don't pass the callbacks to the backend
 		delete options.error;
 		delete options.success;
-        
+
 		// Use CRUDr backend
 		backend.ready(function() {
 			var req = {
+				backend: backend,
 				method: method,
 				model: model.toJSON(),
 				options: options
 			};
-			
-			crudr.sync(backend.socket, req, { error: error, success: success } );
-			
+
+			crudr.sync(req, { error: error, success: success } );
+
 		});
-		
+
 	} else {
 		// Call the original Backbone.sync
 		origSync(method, model, options);
 	}
 };
-	
+
 Backbone.Collection = (function(Parent) {
 	// Override the parent constructor
-	
-    var Child = function() {
+
+	var Child = function() {
 		if (this.backend) {
 			this.backend = crudr.subscribe({
 				el : this,
 				name : this.backend
 			});
 		}
-		
+
 		Parent.apply(this, arguments);
 	};
-	
-    // Inherit everything else from the parent
+
+	// Inherit everything else from the parent
 	return inherit(Parent, Child, [Helpers]);
 })(Backbone.Collection);
 
 Backbone.Model = (function(Parent) {
 	// Override the parent constructor
 	var Child = function() {
-		if (this.backend) {                
+		if (this.backend) {
 			this.backend = crudr.subscribe({
 				el : this,
 				name : this.backend
 			});
 		}
-		
+
 		Parent.apply(this, arguments);
 	};
-	
+
 	// Inherit everything else from the parent
 	return inherit(Parent, Child, [Helpers]);
 })(Backbone.Model);
